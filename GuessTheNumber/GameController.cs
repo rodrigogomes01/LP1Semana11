95bd2b666ceb1985513e6f9b1e4c302578c13c4a
rodrigogomes01 <rodrigogomes091997@gmail.com>
2023-05-26 14:29:44 +0100

    Exercise 2
    Added GameController to the project GuessTheNumber

---
+++ GuessTheNumber/GameController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GuessTheNumber
+{
+    public class GameController
+    {
+        private int targetNumber;
+        private int attempts;
+
+        public Controller(int targetNumber)
+        {
+            this.targetNumber = targetNumber;
+        }
+
+        public void Run(IView view)
+        {
+            view.Start();
+
+            int guess = view.GetInput();
+            while (guess != targetNumber)
+            {
+                attempts++;
+                view.IncorrectGuess(guess, targetNumber);
+                guess = view.GetInput();
+            }
+
+            view.CorrectGuess(attempts);
+            view.End();
+        }
+    }
+}
